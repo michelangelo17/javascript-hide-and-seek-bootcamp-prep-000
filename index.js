f62d113c04f7ac2f3db4b43571d872aae1d08fb0
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-17 13:42:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return (document.querySelector(`${selector}`));
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const liRanks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; liRanks.length > i; i++) {
+   liRanks[i].innerHTML = parseInt(liRanks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+var theDivIsDeep = document.querySelector('#grand-node div div div div')
+return theDivIsDeep
+}
